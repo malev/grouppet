0f42131d9ec99bf3fcff02ffcfd76b9201d7756d
Gustavo Aarón Cárdenas Andrade <sir.gusaar.hiryoken@gmail.com>
2011-03-18 19:41:52 -0300

    Missing files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grouppet::Application.config.secret_token = 'b3dc299734bf858735c239e1730a3a667355776290dc4ea2971c94c57145913fa174970e79e086983d917509b62b37ea2932387db464219af18a7d5b1d48f27b'
