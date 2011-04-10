69e509faa71e6ab6813916f8a9778118169b4a64
Gustavo Aarón Cárdenas Andrade <sir.gusaar.hiryoken@gmail.com>
2011-04-09 21:20:36 -0300

    Removing user groups to implement friends.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   include Clearance::User
 
   # Groups that the user has
-  has_many :groups, :class_name => 'UserGroup', :dependent => :destroy
+  #has_many :groups, :class_name => 'UserGroup', :dependent => :destroy
   # Groups where the user is
-  has_many :user_user_groups, :foreign_key => 'member_id'
-  has_many :member_groups, :source => :user_group, :through => :user_user_groups
+  #has_many :user_user_groups, :foreign_key => 'member_id'
+  #has_many :member_groups, :source => :user_group, :through => :user_user_groups
 end
